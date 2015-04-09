a1790b50ffeeb408189c5464eb291f8ce8131cdd
Anna Do <doannado@gmail.com>
2015-04-08 23:38:20 -0400

    movement with chutes and ladders work, kinda ugly

--- ChutesAndLadder/Assets/Script/PlayerMovement.cs
+++ ChutesAndLadder/Assets/Script/PlayerMovement.cs
@@ -6,11 +6,13 @@ public class PlayerMovement : MonoBehaviour {
 	
 	private float max; 
 	private float min;
+	private int curSpace;
 	
 	// Use this for initialization
 	void Start () {
 		max = 9;
 		min = 0;
+		curSpace = 0;
 	}
 	
 	// Update is called once per frame
@@ -18,7 +20,12 @@ public class PlayerMovement : MonoBehaviour {
 		
 	}
 	
-	public void MoveSpaces(float spaces){
+	public void MoveSpaces(int spaces){
+		if (curSpace + spaces > 100) {
+			print ("over 100");
+			return;		
+		}
+		curSpace += spaces;
 		Vector3 curPos = transform.localPosition;
 		bool right = (curPos.y % 2 == 0);
 		float distX = 0;
@@ -48,6 +55,79 @@ public class PlayerMovement : MonoBehaviour {
 				curPos.x -= spaces;
 			}
 		}
+		curSpace = chutesAndLadders (curSpace);
+		curPos = spacesToCoord (curSpace);
+//		//space to coord
+		print ("cur space " + curSpace);
 		transform.localPosition = curPos;
+		if(curSpace == 100){
+			print ("winner!");
+		}
+	}
+
+	private int chutesAndLadders(int curSpace){
+		switch (curSpace) {
+			case 1:
+				return 38;
+			case 4:
+				return 14;
+			case 9:
+				return 31;
+			case 16:
+				return 6;
+			case 21:
+				return 42;
+			case 28:
+				return 84;
+			case 36:
+				return 44;
+			case 48:
+				return 26;
+			case 49:
+				return 11;
+			case 51:
+				return 67;
+			case 56:
+				return 53;
+			case 62:
+				return 19;
+			case 64:
+				return 60;
+			case 71:
+				return 91;
+			case 80:
+				return 100;
+			case 87:
+				return 24;
+			case 93:
+				return 73;
+			case 95:
+				return 75;
+			case 98:
+				return 78;
+			default:
+				break;
+			}
+		return curSpace;
+	}
+
+	private Vector3 spacesToCoord(int spaces){
+		int y;
+		int x;
+		y = (int)((spaces - 1) / 10);
+		if (y % 2 == 0) {
+			x = ((spaces ) % 10) - 1;
+			if((spaces % 10) == 0){
+				x = 9;
+			}
+		}
+		else{
+			x = 10 - (spaces % 10);
+			if((spaces % 10) == 0){
+				x = 0;
+			}
+		}
+		print ("y " + y);
+		return new Vector3 (x, y, 0);
 	}
 }
