d23cdbbd17db2c8aa7c000a7aa77f2cf021fde8d
Anna Do <doannado@gmail.com>
2015-04-08 22:41:02 -0400

    player movement on board works

--- ChutesAndLadder/Assets/Script/PlayerMovement.cs
+++ ChutesAndLadder/Assets/Script/PlayerMovement.cs
@@ -3,31 +3,52 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	private int max; 
-	private int min;
-
+	
+	private float max; 
+	private float min;
+	
 	// Use this for initialization
 	void Start () {
 		max = 9;
 		min = 0;
+		MoveSpaces (12f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
-	public void MoveSpaces(int spaces){
-		Vector3 temp = transform.position;
-		//even rows move right
-		//odd rows move left
-		if (temp.y % 2 == 0) {
-					
+	
+	public void MoveSpaces(float spaces){
+		Vector3 curPos = transform.localPosition;
+		bool right = (curPos.y % 2 == 0);
+		float distX = 0;
+		float nextX = 0;
+		if (right) {
+			print ("even row");
+			distX = max - curPos.x;
+			if(spaces > distX){
+				print ("reached right bound " + distX);
+				nextX = spaces - distX;
+				curPos.x = max - (nextX - 1);
+				curPos.y++;
+			}
+			else{
+				curPos.x += spaces;
+			}
 		}
 		else{
-		
+			distX = curPos.x - min;
+			if(spaces > distX){
+				print ("reached left bound");
+				nextX = spaces - distX;
+				curPos.x = nextX - 1;
+				curPos.y++;
+			}
+			else{
+				curPos.x -= spaces;
+			}
 		}
-
-
+		transform.localPosition = curPos;
 	}
 }
