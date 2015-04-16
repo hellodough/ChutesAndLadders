1072450e43ed25667fe178fabaab59956c16d2a3
Anna Do <doannado@gmail.com>
2015-04-15 23:03:00 -0400

    online lobby scene created

---
+++ ChutesAndLadder/Assets/Script/Online.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Online : MonoBehaviour {
+
+	public GameObject red;
+	public GameObject green;
+	public GameObject blue;
+	public GameObject yellow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PickColor(int color){
+		switch (color) {
+			case 0:
+				red.GetComponent<Button>().interactable = false;
+				green.GetComponent<Button>().interactable = true;
+				blue.GetComponent<Button>().interactable = true;
+				yellow.GetComponent<Button>().interactable = true;
+				break;
+			case 1:
+				red.GetComponent<Button>().interactable = true;
+				green.GetComponent<Button>().interactable = false;
+				blue.GetComponent<Button>().interactable = true;
+				yellow.GetComponent<Button>().interactable = true;
+				break;
+			case 2:
+				blue.GetComponent<Button>().interactable = false;
+				red.GetComponent<Button>().interactable = true;
+				green.GetComponent<Button>().interactable = true;
+				yellow.GetComponent<Button>().interactable = true;
+				break;
+			case 3:
+				yellow.GetComponent<Button>().interactable = false;
+				red.GetComponent<Button>().interactable = true;
+				green.GetComponent<Button>().interactable = true;
+				blue.GetComponent<Button>().interactable = true;
+				break;
+		}
+	}
+}
